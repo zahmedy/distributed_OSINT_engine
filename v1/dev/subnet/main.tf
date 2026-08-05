12c6df9385bb9cf7b8c87b6d23c7b7cc22fe1907
Zayed Ahmed <zayed967@yahoo.com>
2026-08-05 13:06:13 -0400

    include output to get vpc id

--- v1/dev/subnet/main.tf
+++ v1/dev/subnet/main.tf
@@ -6,3 +6,14 @@
 #     "Name" = "dev-private"
 #   }
 # }
+
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+  config = {
+    path = "../vpc/terraform.tfstate"
+  }
+}
+
+output "test" {
+  value = data.terraform_remote_state.vpc
+}
