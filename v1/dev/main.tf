1c399b3b9be3bb679c18aef96e7b82a334eaa188
Zayed Ahmed <zayed967@yahoo.com>
2026-08-05 11:53:12 -0400

    done

---
+++ v1/dev/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+
+    enable_dns_support  = false
+    enable_dns_hostnames = false
+
+    tags = {
+        Name = "dev-main"
+    }
+}
