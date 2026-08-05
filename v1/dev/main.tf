d10658a76314c69944c8bcfc4ec192d1e4fffd62
Zayed Ahmed <zayed967@yahoo.com>
2026-08-05 12:40:30 -0400

    add subnet

--- v1/dev/main.tf
+++ v1/dev/main.tf
@@ -8,3 +8,12 @@ resource "aws_vpc" "main" {
     Name = var.env
   }
 }
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/19"
+
+  tags = {
+    "Name" = "dev-private"
+  }
+}
