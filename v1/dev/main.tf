15ca359eeef328159496e425232f6c56ec9d567d
Zayed Ahmed <zayed967@yahoo.com>
2026-08-05 12:27:25 -0400

    add varibales

--- v1/dev/main.tf
+++ v1/dev/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-    region = "us-east-2"
+  region = var.region
 }
 
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr_block
 
-    enable_dns_support  = false
-    enable_dns_hostnames = false
+  enable_dns_support   = false
+  enable_dns_hostnames = false
 
-    tags = {
-        Name = "dev-main"
-    }
+  tags = {
+    Name = var.env
+  }
 }
